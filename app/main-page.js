5e231574a948a9308625bbcf1ca81554b6acc3bb
Katturaja Mookkan <katturaja.m@tvsi.com>
2016-05-06 15:48:00 +0530

    tns parralax plugin

---
+++ app/main-page.js
@@ -0,0 +1,8 @@
+"use strict";
+// Event handler for Page "loaded" event attached in main-page.xml
+function pageLoaded(args) {
+    var page = args.object;
+    page.actionBarHidden = true;
+}
+exports.pageLoaded = pageLoaded;
+//# sourceMappingURL=main-page.js.map
